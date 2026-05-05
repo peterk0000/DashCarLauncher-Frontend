b3ff89b146a79df3fb4cb8c969e9a5ac47f96d82
peterk0000 <kpa0400@yandex.ru>
2026-05-05 17:31:20 +0300

    1

--- app/src/main/java/com/example/dashcarlauncher_frontend/MainActivity.java
+++ app/src/main/java/com/example/dashcarlauncher_frontend/MainActivity.java
@@ -2,14 +2,9 @@ package com.example.dashcarlauncher_frontend;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
